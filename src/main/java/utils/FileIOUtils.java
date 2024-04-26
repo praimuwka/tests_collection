a51c0851eca5f0111815a42bf4e6c5bdeefd8739
Prime <kbuschuev@gmail.com>
2024-04-27 00:28:50 +0300

    algo2

--- src/main/java/utils/FileIOUtils.java
+++ src/main/java/utils/FileIOUtils.java
@@ -1,12 +1,7 @@
 package utils;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class FileIOUtils {
 
