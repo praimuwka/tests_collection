07d2adb4147adacc1ec3f7b113db09320aab14ba
Prime <kbuschuev@gmail.com>
2024-05-12 19:19:20 +0300

    init project

---
+++ src/main/java/ru/praimuwka/konustest/KonustestApplication.java
@@ -0,0 +1,11 @@
+package ru.praimuwka.konustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonustestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KonustestApplication.class, args);
+	}
+}
